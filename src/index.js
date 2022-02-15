87479ff2a642c34fe077af3343711dd42354a18d
matovu-farid <matovu90@gmail.com>
2022-02-15 17:41:02 +0300

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Calculator from './components/Calculator/Calculator';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Calculator />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
