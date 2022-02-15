6cc3c74bbde6964aa28f77a9f18ec2b69e06ce97
matovu-farid <matovu90@gmail.com>
2022-02-15 17:32:47 +0300

    Create pages

---
+++ src/pages/Quote.js
@@ -0,0 +1,8 @@
+const Quote = () => (
+  <p>
+    Mathematics is not about numbers, equations, computations,
+    or algorithms: it is about understanding.” – William Paul Thurston.
+  </p>
+);
+
+export default Quote;
