918ac8d3b7006d2d434615f1fa3d9a53bafce969
matovu-farid <matovu90@gmail.com>
2022-02-15 17:53:19 +0300

    Add the heading

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,23 +2,26 @@ import './Navbar.css';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
-  <ul className="nav-list">
-    <li>
-      <NavLink to="/">
-        Home
-      </NavLink>
-    </li>
-    <li>
-      <NavLink to="/calculator">
-        Calculator
-      </NavLink>
-    </li>
-    <li>
-      <NavLink to="/quote">
-        Quote
-      </NavLink>
-    </li>
-  </ul>
+  <div className="nav-bar">
+    <h1>Math Magicians</h1>
+    <ul className="nav-list">
+      <li>
+        <NavLink to="/">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/calculator">
+          Calculator
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/quote">
+          Quote
+        </NavLink>
+      </li>
+    </ul>
+  </div>
 );
 
 export default Navbar;
