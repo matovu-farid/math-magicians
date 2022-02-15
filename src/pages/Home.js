568a9305d1137892903e1fee968b6e2969d1598b
matovu-farid <matovu90@gmail.com>
2022-02-15 20:03:14 +0300

    Add text for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,23 +2,34 @@ const Home = () => (
   <div>
     <h2>Welcome to our page</h2>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-      optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-      obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-      nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-      tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-      quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-      sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-      recusandae alias error harum maxime adipisci amet laborum. Perspiciatis
-      minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit
-      quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur
-      fugiat, temporibus enim commodi iusto libero magni deleniti quod quam
-      consequuntur! Commodi minima excepturi repudiandae velit hic maxime
-      doloremque. Quaerat provident commodi consectetur veniam similique ad
-      earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo
-      fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labor
+      Even though everyone learns mathematics at school, it is difficult
+      to define exactly what mathematics is. Clearly numbers, shapes and
+      equations form part of it, but only a small part compared to the
+      vast space of mathematical concepts and ideas. The best way to
+      understand what mathematics is and what mathematicians work
+      on – is to do mathematics.
+
+      One idea that appears everywhere in mathematics is abstraction.
+      Instead of thinking about particular numbers, shapes, equations
+      or any other objects, mathematicians tend to think about their
+
+      underlying structures and patterns. This means that the results,
+      called Theorems, are more general and provide deeper insight.
+      Another fundamental idea in mathematics is Proof. Mathematicians
+      can’t just say that an idea is true, or test it in a few cases.
+      They need a rigorous and watertight argument to deduce that it
+      is always true. Maybe this makes mathematics more difficult than
+      other sciences, but it also means that mathematicians can obtain
+      absolute and definitive knowledge – which is impossible in any
+      other discipline.
+
+      Despite being so abstract and theoretical, mathematics has countless
+      applications in every possible aspect of life. Without mathematics our
+      civilisation would be little more advanced than the ancient Egyptians:
+      we wouldn’t have governments funded by a tax system, no phones,
+      no television, no computers, no internet and no satellite navigation.
+      The cultural value and the monetary economic value of mathematics
+      are too large to measure.
     </p>
 
   </div>
