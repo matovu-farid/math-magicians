4ba0074f89aafa5642638cc11fc64e2ef2bc6193
matovu-farid <matovu90@gmail.com>
2022-02-15 17:49:22 +0300

    Add nav links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 import Calculator from './pages/Calculator';
 import Quote from './pages/Quote';
 // import Home from './pages/Home';
 
 const App = () => (
-  <Routes>
-    <Route path="/" element={<Calculator />} />
-    <Route path="/calculator" element={<Calculator />} />
-    <Route path="/quote" element={<Quote />} />
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Calculator />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quote />} />
 
-  </Routes>
+    </Routes>
+  </>
 
 );
 
