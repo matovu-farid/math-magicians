aba522216fa8df5c118b40f162758fe99a28f006
matovu-farid <matovu90@gmail.com>
2022-02-15 19:11:27 +0300

    Add icons

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,32 @@
 import './Navbar.css';
 import { NavLink } from 'react-router-dom';
 
+import { ImHome2 } from 'react-icons/im';
+import { BsCalculatorFill, BsFillChatLeftQuoteFill } from 'react-icons/bs';
+
 const Navbar = () => (
   <div className="nav-bar">
     <h1>Math Magicians</h1>
     <ul className="nav-list">
       <li>
-        <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
-          Home
+        <NavLink to="/" className={({ isActive }) => (isActive ? 'active navlink' : 'inactive navlink')}>
+          <ImHome2 />
+          <span>Home</span>
+
         </NavLink>
       </li>
       <li>
-        <NavLink to="/calculator" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
-          Calculator
+        <NavLink to="/calculator" className={({ isActive }) => (isActive ? 'active navlink' : 'inactive navlink')}>
+          <BsCalculatorFill />
+          <span>Calculator</span>
+
         </NavLink>
       </li>
       <li>
-        <NavLink to="/quote" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
-          Quote
+        <NavLink to="/quote" className={({ isActive }) => (isActive ? 'active navlink' : 'inactive navlink')}>
+          <BsFillChatLeftQuoteFill />
+          <span>Quote</span>
+
         </NavLink>
       </li>
     </ul>
