3004460df9294adf0dc342069c7d5b1c7a99c555
matovu-farid <matovu90@gmail.com>
2022-02-17 17:22:24 +0300

    Add nav bar lists

---
+++ src/components/Navbar/__tests__/Navbar.test.js
@@ -0,0 +1,29 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from '../Navbar';
+
+describe.skip('navbar tests', () => {
+  const MockNavBar = () => (
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
+  it('should display correct value', () => {
+    render(<MockNavBar />);
+    const links = screen.queryAllByRole('button');
+
+    expect(links.length).toBe(3);
+  });
+
+  it('home should have an active class', () => {
+    render(<MockNavBar />);
+    const homeLink = screen.queryByRole('link', { name: 'Home' });
+    expect(homeLink).toHaveClass('active');
+  });
+  it('home should have an active class', () => {
+    render(<MockNavBar />);
+    const homeLink = screen.queryByRole('link', { name: /calculator/i });
+    expect(homeLink).not.toHaveClass('active');
+  });
+});
